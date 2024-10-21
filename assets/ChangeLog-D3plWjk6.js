875fb83b3a00c784475e45278eb315ecab3e6e1e
jony wu <37248488+jonyworker@users.noreply.github.com>
2024-10-21 11:52:39 +0800

    Updates

---
+++ assets/ChangeLog-D3plWjk6.js
@@ -0,0 +1,19 @@
+import{af as n,ag as a,ah as s}from"./index-CtjXmIsv.js";import{u as o}from"./index-DZhqMLwO.js";import"./iframe-BIuOw2Wo.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-DrFu-skq.js";const i=`## V1.0.0 (2024-10-21)\r
+\r
+✨ New Feature\r
+\r
+---\r
+\r
+🆙update\r
+\r
+- 同步學長版 storybook\r
+\r
+---\r
+\r
+🪲 Bug Fix\r
+`;function e(r){const t={h1:"h1",...o(),...r.components};return n.jsxs(n.Fragment,{children:[`
+`,`
+`,`
+`,n.jsx(a,{title:"Changelog"}),`
+`,n.jsx(t.h1,{id:"changelog",children:"Changelog"}),`
+`,n.jsx(s,{children:i})]})}function g(r={}){const{wrapper:t}={...o(),...r.components};return t?n.jsx(t,{...r,children:n.jsx(e,{...r})}):e(r)}export{g as default};
---
+++ assets/index-DIwgRSk_.js
@@ -0,0 +1,1 @@
+import{M as s,u as M}from"./index-DZhqMLwO.js";import"./index-CtjXmIsv.js";import"./iframe-BIuOw2Wo.js";import"../sb-preview/runtime.js";import"./index-i39XpuQg.js";import"./index-DrFu-skq.js";export{s as MDXProvider,M as useMDXComponents};
